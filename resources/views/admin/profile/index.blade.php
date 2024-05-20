e641644786283f695a14c6230ae8fb6736c41b50
vereskyer <vereskyer@gmail.com>
2024-05-20 13:08:07 +0900

    重新放uer & vendor dashboard template

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -65,24 +65,6 @@
                                 </div>
                             </label>
 
-
-                            <label class="block text-sm mt-4">
-                                <span class="text-gray-700 dark:text-gray-400">Password</span>
-                                <!-- focus-within sets the color for the icon when input is focused -->
-                                <div
-                                    class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-                                    <input
-                                        class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                                        type="password" />
-                                    <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-                                        </svg>
-                                    </div>
-                                </div>
-                            </label>
                             <!-- You should use a button here, as the anchor is only used for the example  -->
                             <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
                                 >
--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -12,15 +12,27 @@
                         </h1>
 
                         {{-- update profile  --}}
-                        <form>
+                        <form action="{{ route('admin.profile.update') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+
                             <label class="block text-sm">
+                                <span class="text-gray-700 dark:text-gray-400">Image</span>
+                                <!-- focus-within sets the color for the icon when input is focused -->
+                                <div
+                                    class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+                                    <img class="w-12 h-12 rounded-full" src="{{ asset(Auth::user()->image) }}" alt="">
+                                </div>
+                                <input type="file" name="image" class="" />
+                            </label>
+
+                            <label class="block text-sm mt-4">
                                 <span class="text-gray-700 dark:text-gray-400">Name</span>
                                 <!-- focus-within sets the color for the icon when input is focused -->
                                 <div
                                     class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                                     <input
                                         class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                                        value="{{ Auth::user()->name }}" />
+                                        value="{{ Auth::user()->name }}"  name="name"/>
                                     <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
 
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -39,7 +51,7 @@
                                     class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                                     <input
                                         class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                                        value="{{ Auth::user()->email }}" />
+                                        value="{{ Auth::user()->email }}" name="email" type="email"/>
                                     <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
                                         <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                                             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
@@ -53,29 +65,11 @@
                                 </div>
                             </label>
 
-
-                            <label class="block text-sm mt-4">
-                                <span class="text-gray-700 dark:text-gray-400">Password</span>
-                                <!-- focus-within sets the color for the icon when input is focused -->
-                                <div
-                                    class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
-                                    <input
-                                        class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-                                        type="password" />
-                                    <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-                                        </svg>
-                                    </div>
-                                </div>
-                            </label>
                             <!-- You should use a button here, as the anchor is only used for the example  -->
-                            <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                                href="./login.html">
+                            <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                                >
                                 Update Changes
-                            </a>
+                            </button>
                         </form>
 
                         <hr class="my-8" />
@@ -85,14 +79,14 @@
 
                             @csrf
 
-                            <label class="block mt-4 text-sm">
+                            <label class="block text-sm mt-4">
                                 <span class="text-gray-700 dark:text-gray-400">Password</span>
+                                <!-- focus-within sets the color for the icon when input is focused -->
                                 <div
                                     class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                                     <input
-                                        class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                                        type="password" name="current_password" />
-
+                                        class="block w-full pl-10 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                                        type="password" name="current_password"/>
                                     <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -108,7 +102,7 @@
                                 <div
                                     class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                                     <input
-                                        class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                                        class="block w-full pl-10 mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                                         type="password" name="password" />
                                     <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -125,7 +119,7 @@
                                 <div
                                     class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
                                     <input
-                                        class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                                        class="block w-full pl-10 mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                                         name="password_confirmation" type="password" />
 
                                     <div class="absolute inset-y-0 flex items-center ml-3 pointer-events-none">

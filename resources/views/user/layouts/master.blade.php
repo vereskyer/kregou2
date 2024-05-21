879121f3e6f0b1f0152116b9cb16a730a0e2e748
vereskyer <vereskyer@gmail.com>
2024-05-21 16:27:50 +0900

    user dashboard 留白問題fix

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -26,7 +26,7 @@
 </head>
 
 <body>
-    <div class="flex h-full bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
+    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
         <!-- Desktop sidebar -->
         @include('user.layouts.sidebar')
 

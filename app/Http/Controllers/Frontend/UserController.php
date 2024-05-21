ca8d1085e0eee5bbf98ea2e7f84f4206e15f240b
vereskyer <vereskyer@gmail.com>
2024-05-21 15:32:39 +0900

    controllers中將user的shoporders帶進來

--- app/Http/Controllers/Frontend/UserController.php
+++ app/Http/Controllers/Frontend/UserController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Shoporder;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('user.dashboard');
+        $shoporders = Shoporder::where('user_id', Auth::user()->id)->paginate(10);
+        return view('user.dashboard', compact('shoporders'));
     }
 }

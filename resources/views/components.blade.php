42e94c8bc05b31e9aff9353244dac536f85edbe1
vereskyer <vereskyer@gmail.com>
2024-06-01 00:08:50 +0900

    找到buildings display 模板-> 作爲about us介紹來用

--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -663,7 +663,8 @@
                 </h1>
                 <p
                     class="mt-10 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    如果有其他可以提供參考的信息，可以放這裏Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+                    如果有其他可以提供參考的信息，可以放這裏Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
+                    commodo. Elit sunt amet fugiat veniam occaecat.
                 </p>
                 <!-- Button Section -->
                 <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
@@ -703,7 +704,179 @@
         </div>
     </section>
 
+    {{-- 阿拉伯風格的一個組件 --}}
+    <div dir="rtl">
+        <div class="">
+            <div
+                class="mx-auto h-full px-4 py-10 sm:max-w-xl md:max-w-full md:px-24 md:py-36 lg:max-w-screen-xl lg:px-8">
+                <div class="flex flex-col items-center justify-between lg:flex-row md:gap-10  gap-5">
+                    <div class="relative  lg:block lg:w-1/2">
+                        <div
+                            class="relative my-10 flex h-full flex-col overflow-hidden rounded-2xl bg-white text-gray-600 shadow-lg ring-1 ring-gray-200">
 
+                            <h2 class="m-5 bg-teal-500 p-3 rounded-xl text-center  text-xl font-bold">
+                                和搜尋引擎一起使用</h2>
+
+                            <div class="flex-auto p-6">
+                                <div class="relative flex flex-col justify-center">
+                                    <div class="absolute right-4 h-full border-l-2"></div>
+                                    <div class="relative mb-4">
+                                        <span
+                                            class="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 p-4 text-center text-base font-semibold text-white shadow">
+                                            1</span>
+                                        <div class="mr-12 w-auto pt-1">
+                                            <h6 class="text-sm font-semibold text-blue-900">111春末秋初</h6>
+                                            <p class="mt-1 text-xs text-gray-500">韓國年代末，韓國在線
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div class="relative mb-4">
+                                        <span
+                                            class="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 p-4 text-center text-base font-semibold text-white shadow">
+                                            2</span>
+                                        <div class="mr-12 w-auto pt-1">
+                                            <h6 class="text-sm font-semibold text-blue-900">發展韓國東大門
+                                            </h6>
+                                            <p class="mt-1 text-xs text-gray-500">韓國東大門是韓國最重要的大門</p>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="relative mb-4">
+                                        <span
+                                            class="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 p-4 text-center text-base font-semibold text-white shadow">
+                                            3</span>
+                                        <div class="mr-12 w-auto pt-1">
+                                            <h6 class="text-sm font-semibold text-blue-900">porky童裝商場</h6>
+                                            <p class="mt-1 text-xs text-gray-500">爲了更好的發展</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="relative mb-4">
+                                        <span
+                                            class="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 p-4 text-center text-base font-semibold text-white shadow">
+                                            4</span>
+                                            <div class="mr-12 w-auto pt-1">
+                                                <h6 class="text-sm font-semibold text-blue-900">porky童裝商場</h6>
+                                                <p class="mt-1 text-xs text-gray-500">爲了更好的發展</p>
+                                            </div>
+                                    </div>
+
+                                    <div class="relative mb-4">
+                                        <span
+                                            class="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 p-4 text-center text-base font-semibold text-white shadow">
+                                            5</span>
+                                            <div class="mr-12 w-auto pt-1">
+                                                <h6 class="text-sm font-semibold text-blue-900">porky童裝商場</h6>
+                                                <p class="mt-1 text-xs text-gray-500">爲了更好的發展</p>
+                                            </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative  lg:block lg:w-1/2">
+
+                        <img src="https://images.pexels.com/photos/23946968/pexels-photo-23946968/free-photo-of-two-people-standing-on-a-street-looking-at-a-map.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            class="w-5/6">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    {{-- buildings 展示頁面 --}}
+    <div class="w-full dark:bg-gray-800">
+
+        <div class="mx-auto max-w-7xl px-6 lg:px-8 pt-8">
+            <div class="mx-auto max-w-2xl text-center">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">From The Blog</h2>
+                <p class="mt-2 text-lg leading-8 text-gray-600 dark:text-gray-300">
+                    Dive into the latest in technology with our insightful blog posts.
+                </p>
+            </div>
+            <div
+                class="mx-auto mt-8 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-12 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
+                <!-- First blog post -->
+                <article
+                    class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+                    <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzA0MDhfMTc2/MDAxNDkxNjI2OTcwNDE4.uqmlayUgsurteZh-WW8fsJGTh-ZWDm89gtZIpz4d1DMg.T1MtVTw-3ypZwvYmZh-9KQNV7uGXUES9EtKbQ4jbo4Mg.JPEG.2nd_bom/image_4488175541491626827704.jpg?type=w800" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+                    <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+                    <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                    <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300"><time
+                            datetime="2023-10-11" class="mr-8">APM 에피엠</time>
+                        <div class="-ml-4 flex items-center gap-x-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+                              </svg>
+                              
+                              
+                            <div class="flex gap-x-2.5">
+                                東大門女裝
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
+                        <span class="absolute inset-0"></span>
+                        東大門經營最久，最具影響力的批發商場之一。B1~3F是女裝，4F~7F是男裝。
+                        參考：
+                        <a href="https://prjewel.com/zh/dongdaemun-wholesale-market-guide/">https://prjewel.com/zh/dongdaemun-wholesale-market-guide/</a>
+                    </h3>
+                </article>
+                <!-- Second blog post -->
+                <article
+                    class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+                    <img src="https://images.unsplash.com/photo-1639322537228-f710d846310a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxibG9jayUyMGNoYWlufGVufDB8MHx8fDE3MTI3NTMxNjd8MA&ixlib=rb-4.0.3&q=80&w=1080" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+                    <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+                    <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                    <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300"><time
+                            datetime="2023-09-25" class="mr-8">Sept 25, 2023</time>
+                        <div class="-ml-4 flex items-center gap-x-4"><svg viewBox="0 0 2 2"
+                                class="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
+                                <circle cx="1" cy="1" r="1"></circle>
+                            </svg>
+                            <div class="flex gap-x-2.5">
+                                <img src="https://randomuser.me/api/portraits/women/2.jpg" alt="" class="h-6 w-6 flex-none rounded-full bg-white/10">Jane
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
+                        <a href="/tech-blog/post2"><span class="absolute inset-0"></span>The Rise of Blockchain Technology:
+                            A Comprehensive Guide</a>
+                    </h3>
+                </article>
+                <!-- Third blog post -->
+                <article
+                    class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 dark:bg-gray-700 px-8 py-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
+                    <img src="https://images.unsplash.com/photo-1666112835156-c65bb806ac73?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxNXx8cXVhbnR1bSUyMGNvbXB1dGluZ3xlbnwwfDB8fHwxNzEyNzUzMTk2fDA&ixlib=rb-4.0.3&q=80&w=1080" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+                    <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+                    <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
+                    <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300"><time
+                            datetime="2023-09-24" class="mr-8">Sept 24, 2023</time>
+                        <div class="-ml-4 flex items-center gap-x-4"><svg viewBox="0 0 2 2"
+                                class="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
+                                <circle cx="1" cy="1" r="1"></circle>
+                            </svg>
+                            <div class="flex gap-x-2.5">
+                                <img src="https://randomuser.me/api/portraits/men/4.jpg" alt="" class="h-6 w-6 flex-none rounded-full bg-white/10">Michael
+                            </div>
+                        </div>
+                    </div>
+                    <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
+                        <a href="/tech-blog/post3"><span class="absolute inset-0"></span>How Quantum Computing Will
+                            Revolutionize Data Security</a>
+                    </h3>
+                </article>
+                <!-- More blog posts can be added similarly -->
+            </div>
+        </div>
     
+    </div> 
 
     @include('layouts.footer')
--- resources/views/user/namecard.blade.php
+++ resources/views/user/namecard.blade.php
@@ -1,30 +1,40 @@
 @extends('user.layouts.master')
 
 @section('content')
-    <main>
+    <main class="ml-10 mt-10 w-3/4">
         <!-- 引入特定版本或配置的 Tailwind CSS -->
         {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet"> --}}
         <div class="card card-side bg-base-100 shadow-xl">
-            <label for="my-drawer-2" class="drawer-button btn btn-primary mt-10">東大門女裝</label>
+            <label for="my-drawer-2" class="ml-3 drawer-button btn btn-primary mt-10">東大門女裝</label>
             <figure class="pl-5">
                 <img src="{{ asset('uploads/namecard.jpg') }}"
                     class="w-40 h-40 rounded-lg" alt="Movie" />
             </figure>
             <div class="card-body">
-                <h2 class="card-title bg-green-500 w-1/5 text-center">마이클 - {{ $user->name }}</h2>
+                <h2 class="card-title text-center">
+                    <span class="border border-green-700 inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-lg font-large text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                        마이클 - {{ $user->name }}
+                    </span>
+
+                    </h2>
                 <p>이름으로 적어주세요</p>
                 <h3 class="card-title">픽업 삼촌: 010.8255.7594</h3>
             </div>
         </div>
 
         <div class="card card-side bg-base-100 shadow-xl mt-8">
-            <label for="my-drawer-2" class="drawer-button btn btn-primary mt-10">南大門童裝</label>
+            <label for="my-drawer-2" class="ml-3 drawer-button btn btn-primary mt-10">南大門童裝</label>
             <figure class="pl-5">
                 <img src="{{ asset('uploads/namecard.jpg') }}"
                     class="w-40 h-40 rounded-lg" alt="Movie" />
             </figure>
             <div class="card-body">
-                <h2 class="card-title bg-green-500 w-1/5 text-center">마이클 - {{ $user->name }}</h2>
+                <h2 class="card-title text-center">
+                    <span class="border border-green-700 inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-lg font-large text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                        마이클 - {{ $user->name }}
+                    </span>
+
+                    </h2>
                 <p>이름으로 적어주세요</p>
                 <h3 class="card-title">픽업 삼촌: 010.4654.0613</h3>
             </div>
@@ -32,14 +42,19 @@
 
 
         <div class="card card-side bg-base-100 shadow-xl mt-8">
-            <label for="my-drawer-2" class="drawer-button btn btn-primary mt-10">快遞發貨</label>
+            <label for="my-drawer-2" class="ml-3 drawer-button btn btn-primary mt-10    ">快遞發貨</label>
             <figure class="pl-5">
                 <img src="{{ asset('uploads/namecard.jpg') }}"
                     class="w-40 h-40 rounded-lg" alt="Movie" />
             </figure>
             <div class="card-body">
-                <h2 class="card-title bg-green-500 w-1/5 text-center">마이클 - {{ $user->name }}</h2>
-                <p>이름으로 적어주세요</p>
+                <h2 class="card-title text-center">
+                    <span class="border border-green-700 inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-lg font-large text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                        마이클 - {{ $user->name }}
+                    </span>
+
+                    </h2>
+                    <p>이름으로 적어주세요</p>
                 <h3 class="card-title">전화: 010.5598.5115</h3>
                 <h3 class="card-title">주소: 서울 동대문구 한천로 440 2층 천한무역</h3>
                 <h3 class="card-title">우편번호: 02423</h3>

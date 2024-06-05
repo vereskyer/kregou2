6de9f2f7f45ffcc966714b35aedcaafdd7160d33
vereskyer <vereskyer@gmail.com>
2024-06-05 17:54:25 +0900

    更新東大門取貨說明的數字svg

--- resources/views/frontend/dongdaemun-pickup.blade.php
+++ resources/views/frontend/dongdaemun-pickup.blade.php
@@ -4,24 +4,27 @@
 <main class="mt-6">
     @include('layouts.hero')
     {{-- hero section end --}}
- 
+
     {{-- 視頻播放器-steps --}}
     <div class="p-4 max-w-xl mx-auto dark:bg-gray-800">
 
         <h3 class="font-heading dark:text-gray-100 mb-8 text-xl font-bold lg:text-2xl">
             東大門女裝，南大門童裝檔口訂貨取貨說明
         </h3>
-    
+
         <div class="flex">
             <div class="mr-4 flex flex-col items-center">
                 <div>
-                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-blue-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="h-6 w-6 text-blue-800 dark:text-slate-200">
-                            <path d="M12 5v14"></path>
-                            <path d="M18 13l-6 6"></path>
-                            <path d="M6 13l6 6"></path>
+                    <div class="flex h-10 w-10 items-center justify-center rounded-full  border-blue-900">
+                        <svg fill="#56B48C" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <title>number10</title>
+                                <path
+                                    d="M16 3c-7.18 0-13 5.82-13 13s5.82 13 13 13 13-5.82 13-13-5.82-13-13-13zM17.757 22.536h-2.469v-9.305c-0.901 0.841-1.964 1.463-3.188 1.867v-2.234c0.644-0.211 1.344-0.612 2.099-1.202s1.273-1.278 1.555-2.064h2.003v12.938z">
+                                </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
@@ -29,28 +32,31 @@
             </div>
             <div class="pt-1 pb-8">
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
-                    <span class="text-blue-600">➊</span>
+
                     首先需要在我們的buy系統，FB登陸（賬戶資金管理都在buy系統）-->
                     <a href="https://s9.buyplus1.com.tw/b/10214181515192392/" target="_blank" class="text-blue-600">
                         點擊註冊</a>
                 </p>
-    
+
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
                     Buy系統登陸以後，請聯繫我們開通賬號。
                 </p>
             </div>
         </div>
-    
+
         <div class="flex">
             <div class="mr-4 flex flex-col items-center">
                 <div>
-                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-blue-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="h-6 w-6 text-blue-800 dark:text-slate-200">
-                            <path d="M12 5v14"></path>
-                            <path d="M18 13l-6 6"></path>
-                            <path d="M6 13l6 6"></path>
+                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-blue-900">
+                        <svg fill="#56B48C" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <title>number11</title>
+                                <path
+                                    d="M16 3c-7.18 0-13 5.82-13 13s5.82 13 13 13 13-5.82 13-13-5.82-13-13-13zM20.342 20.426v2.297h-8.656c0.093-0.867 0.374-1.688 0.843-2.465 0.468-0.776 1.393-1.807 2.774-3.090 1.111-1.037 1.793-1.74 2.045-2.109 0.34-0.51 0.51-1.014 0.51-1.512 0-0.551-0.147-0.975-0.441-1.271s-0.7-0.444-1.219-0.444c-0.512 0-0.92 0.156-1.223 0.467s-0.478 0.827-0.523 1.549l-2.469-0.247c0.146-1.359 0.605-2.335 1.378-2.928s1.739-0.888 2.898-0.888c1.27 0 2.268 0.343 2.994 1.028s1.089 1.538 1.089 2.557c0 0.58-0.104 1.132-0.312 1.656s-0.537 1.074-0.988 1.647c-0.299 0.38-0.839 0.929-1.621 1.644-0.781 0.714-1.276 1.188-1.484 1.422s-0.376 0.463-0.505 0.686h4.91z">
+                                </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
@@ -58,24 +64,26 @@
             </div>
             <div class="pt-1 pb-8">
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
-                    <span class="text-blue-600">➋</span>
                     其次，需要註冊這個網站的賬號（取貨訂單提交會這裏管理） -->
                     <a href="https://www.daigou82.com/register" target="_blank" class="text-blue-600">
                         點擊註冊</a>
                 </p>
             </div>
         </div>
-    
+
         <div class="flex">
             <div class="mr-4 flex flex-col items-center">
                 <div>
-                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-blue-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="h-6 w-6 text-blue-800 dark:text-slate-200">
-                            <path d="M12 5v14"></path>
-                            <path d="M18 13l-6 6"></path>
-                            <path d="M6 13l6 6"></path>
+                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-blue-900">
+                        <svg fill="#56B48C" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <title>number12</title>
+                                <path
+                                    d="M16 3c-7.18 0-13 5.82-13 13s5.82 13 13 13 13-5.82 13-13-5.82-13-13-13zM18.995 21.357c-0.826 0.797-1.854 1.194-3.086 1.194-1.166 0-2.133-0.335-2.9-1.005-0.769-0.67-1.214-1.545-1.337-2.627l2.391-0.289c0.076 0.607 0.281 1.071 0.616 1.393 0.333 0.321 0.738 0.482 1.213 0.482 0.51 0 0.939-0.194 1.289-0.582 0.348-0.387 0.522-0.909 0.522-1.566 0-0.621-0.167-1.115-0.501-1.479-0.335-0.364-0.742-0.545-1.223-0.545-0.317 0-0.695 0.062-1.136 0.184l0.272-1.997c0.668 0.018 1.178-0.127 1.529-0.434s0.526-0.715 0.526-1.224c0-0.433-0.128-0.777-0.385-1.035-0.258-0.257-0.599-0.386-1.025-0.386-0.421 0-0.779 0.146-1.077 0.438s-0.479 0.72-0.544 1.281l-2.281-0.386c0.158-0.782 0.397-1.407 0.717-1.875s0.765-0.835 1.336-1.103 1.212-0.401 1.921-0.401c1.213 0 2.186 0.387 2.918 1.161 0.604 0.633 0.905 1.348 0.905 2.145 0 1.131-0.619 2.034-1.858 2.708 0.739 0.158 1.33 0.513 1.772 1.063 0.443 0.551 0.664 1.215 0.664 1.994 0.001 1.132-0.412 2.095-1.238 2.891z">
+                                </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
@@ -83,7 +91,6 @@
             </div>
             <div class="pt-1 pb-8">
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
-                    <span class="text-blue-600">➌</span>
                     第二步註冊以後 -->
                     <a href="https://www.daigou82.com/login" target="_blank" class="text-blue-600">
                         點擊登陸</a>
@@ -91,22 +98,25 @@
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
                     在用戶中心，點擊 Shop Orders，就可以看到"添加新的取貨"
                 </p>
-    
+
                 <img src="{{ asset('uploads/shoporder.png') }}" alt="daigou82.com" class="w-full mt-5">
-    
+
             </div>
         </div>
-    
+
         <div class="flex">
             <div class="mr-4 flex flex-col items-center">
                 <div>
-                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-blue-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="h-6 w-6 text-blue-800 dark:text-slate-200">
-                            <path d="M12 5v14"></path>
-                            <path d="M18 13l-6 6"></path>
-                            <path d="M6 13l6 6"></path>
+                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-blue-900">
+                        <svg fill="#56B48C" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <title>number13</title>
+                                <path
+                                    d="M16.359 17.583v-4.405l-2.971 4.405h2.971zM16 3c-7.18 0-13 5.82-13 13s5.82 13 13 13 13-5.82 13-13-5.82-13-13-13zM20.344 19.739h-1.594v2.595h-2.391v-2.595h-5.281v-2.147l5.598-8.196h2.074v8.187h1.594v2.156z">
+                                </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
@@ -114,28 +124,30 @@
             </div>
             <div class="pt-1 pb-8">
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
-                    <span class="text-blue-600">➍</span>
                     添加新的取貨，如果我們數據庫中已經保存了相關的檔口。那麼只要輸入部分檔口的名稱，就可以自動填入表格。
                     如果沒有的話，需要手工添加表格相關的內容。或者也可以聯繫我們添加相關的檔口。
                 </p>
-    
+
                 <video class="w-full rounded-lg" style="height: 80%;" controls>
                     <source src="{{ asset('uploads/pickup-dongdaemun.webm') }}" type="video/webm" />
                     Your browser does not support the video tag.
                 </video>
             </div>
         </div>
-    
+
         <div class="flex">
             <div class="mr-4 flex flex-col items-center">
                 <div>
-                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-blue-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="h-6 w-6 text-blue-800 dark:text-slate-200">
-                            <path d="M12 5v14"></path>
-                            <path d="M18 13l-6 6"></path>
-                            <path d="M6 13l6 6"></path>
+                    <div class="flex h-10 w-10 items-center justify-center rounded-full border-blue-900">
+                        <svg fill="#56B48C" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <title>number14</title>
+                                <path
+                                    d="M16 3c-7.18 0-13 5.82-13 13s5.82 13 13 13 13-5.82 13-13-5.82-13-13-13zM19.546 20.68c-0.839 1.137-2.003 1.705-3.492 1.705-1.19 0-2.16-0.319-2.911-0.959-0.75-0.641-1.199-1.502-1.346-2.582l2.469-0.256c0.070 0.559 0.278 1 0.623 1.324 0.345 0.326 0.742 0.488 1.192 0.488 0.515 0 0.949-0.209 1.307-0.627 0.355-0.418 0.534-1.049 0.534-1.893 0-0.79-0.178-1.383-0.532-1.778-0.355-0.395-0.818-0.593-1.388-0.593-0.71 0-1.348 0.312-1.911 0.938l-1.997-0.289 1.266-6.711h6.531v2.312h-4.658l-0.397 2.192c0.553-0.273 1.117-0.411 1.693-0.411 1.099 0 2.030 0.399 2.795 1.196s1.146 1.832 1.146 3.103c-0.001 1.062-0.309 2.009-0.924 2.841z">
+                                </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
@@ -143,31 +155,33 @@
             </div>
             <div class="pt-1 pb-8">
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
-                    <span class="text-blue-600">➎</span>
                     第二天可以查看取貨訂單的狀態。沒有標註狀態的，即代表已經正常取貨。
                 </p>
-    
+
                 <img src="{{ asset('uploads/shoporder-status.png') }}" alt="daigou82.com" class="w-full mt-5">
 
             </div>
         </div>
-    
+
         <div class="flex">
             <div class="mr-4 flex flex-col items-center">
                 <div>
-                    <div
-                        class="flex h-10 w-10 items-center justify-center rounded-full border-2 border-blue-900 bg-blue-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="h-6 w-6 text-white dark:text-slate-200">
-                            <path d="M5 12l5 5l10 -10"></path>
+                    <div class="flex h-10 w-10 items-center justify-center rounded-full">
+                        <svg fill="#56B48C" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <title>number15</title>
+                                <path
+                                    d="M16.062 16.037c-0.476 0-0.877 0.186-1.205 0.558-0.329 0.372-0.493 0.921-0.493 1.647 0 0.803 0.185 1.422 0.555 1.858 0.369 0.437 0.791 0.655 1.267 0.655 0.457 0 0.839-0.18 1.144-0.537s0.457-0.942 0.457-1.757c0-0.838-0.164-1.451-0.492-1.841s-0.74-0.583-1.233-0.583zM16 3c-7.18 0-13 5.82-13 13s5.82 13 13 13 13-5.82 13-13-5.82-13-13-13zM19.047 21.607c-0.773 0.797-1.768 1.194-2.98 1.194-1.302 0-2.372-0.505-3.21-1.516s-1.258-2.667-1.258-4.97c0-2.361 0.438-4.063 1.312-5.106s2.008-1.564 3.404-1.564c0.979 0 1.79 0.275 2.432 0.825 0.643 0.551 1.052 1.349 1.228 2.396l-2.391 0.263c-0.059-0.49-0.212-0.853-0.458-1.086-0.247-0.234-0.567-0.351-0.961-0.351-0.523 0-0.966 0.233-1.327 0.701s-0.589 1.44-0.683 2.92c0.615-0.726 1.381-1.089 2.297-1.089 1.032 0 1.916 0.392 2.652 1.176s1.104 1.797 1.104 3.038c0 1.318-0.387 2.374-1.161 3.169z">
+                                </path>
+                            </g>
                         </svg>
                     </div>
                 </div>
             </div>
             <div class="pt-1">
                 <p class="mb-2 text-xl font-bold text-gray-900 dark:text-slate-300">
-                    <span class="text-blue-600">➏</span>
                     正常取貨成功的，會入單在buy系統，進行扣款部分的處理。-->
                     <a href="https://s9.buyplus1.com.tw/b/10214181515192392/" target="_blank" class="text-blue-600">
                         登陸buy查看賬戶明細</a>
@@ -175,7 +189,7 @@
             </div>
         </div>
     </div>
-    
+
 
     <script>
         document.addEventListener("alpine:init", () => {

1e28b22a10db914d93a314e5c7d3edba168824d5
vereskyer <vereskyer@gmail.com>
2024-06-25 21:41:27 +0900

    user panel sider bar show site home page

--- resources/views/user/layouts/sidebar.blade.php
+++ resources/views/user/layouts/sidebar.blade.php
@@ -100,11 +100,10 @@
 
         </ul>
         <div class="px-6 my-6">
-            <button
-                class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                Create account
-                <span class="ml-2" aria-hidden="true">+</span>
-            </button>
+            <a href="{{ route('welcome') }}"
+                class="button flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                返回網站首頁
+            </a>
         </div>
     </div>
 </aside>
@@ -217,11 +216,10 @@
 
         </ul>
         <div class="px-6 my-6">
-            <button
+            <a href="{{ route('welcome') }}"
                 class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                Create account
-                <span class="ml-2" aria-hidden="true">+</span>
-            </button>
+                返回網站首頁
+            </a>
         </div>
     </div>
 </aside>
